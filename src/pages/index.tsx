83a7cbc5e9a2a7052220ac52ab6641966e643791
Samuel Newman <mozzius@protonmail.com>
2022-10-02 12:02:40 +0100

    loading animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,8 +28,10 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex h-screen bg-gray-50">
         <div className="m-auto flex w-full max-w-sm flex-col">
-          <h1 className="text-center text-xl font-semibold text-slate-700">tincy.link</h1>
-          <div className="mt-2 flex w-full flex-col rounded p-4 shadow-lg bg-white">
+          <h1 className="text-center text-xl font-semibold text-slate-700">
+            tincy.link
+          </h1>
+          <div className="mt-2 flex w-full flex-col rounded bg-white p-4 shadow-lg">
             <input
               type="url"
               value={url}
@@ -40,19 +42,28 @@ const Home: NextPage = () => {
             <button
               className="mt-4 h-10 rounded bg-slate-700 text-white"
               onClick={onCreate}
+              disabled={create.isLoading}
             >
               Create a tincy link!
             </button>
           </div>
-          {link && (
-            <div
-              className="mt-4 rounded bg-white shadow-lg p-4 relative cursor-pointer"
-              onClick={() => onCopy(link.slug)}
-            >
-              <p className="text-sm">https://tincy.link/{link.slug}</p>
-              <p className="text-xs text-gray-600">{link.url}</p>
-              <CopyIcon className="absolute right-4 top-1/2 transform -translate-y-1/2" />
+          {create.isLoading ? (
+            <div className="relative mt-4 cursor-pointer rounded bg-white p-4 shadow-lg">
+              <div className="h-4 w-32 mb-2 bg-gray-200 rounded animate-pulse" />
+              <div className="h-3 w-24 bg-gray-200 rounded animate-pulse" />
+              <div className="absolute right-4 top-1/2 h-4 w-4 -translate-y-1/2 transform bg-gray-200 rounded animate-pulse" />
             </div>
+          ) : (
+            link && (
+              <div
+                className="relative mt-4 cursor-pointer rounded bg-white p-4 shadow-lg"
+                onClick={() => onCopy(link.slug)}
+              >
+                <p className="mb-1">https://tincy.link/{link.slug}</p>
+                <p className="text-sm text-gray-600">{link.url}</p>
+                <CopyIcon className="absolute right-4 top-1/2 -translate-y-1/2 transform" />
+              </div>
+            )
           )}
         </div>
       </main>
