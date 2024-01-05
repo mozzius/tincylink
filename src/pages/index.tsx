ec43afbb87e6708e9da057f2050f41080d43c9a7
Samuel Newman <mozzius@protonmail.com>
2024-01-05 11:49:19 +0000

    multiselect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,16 +8,18 @@ import { api } from "../utils/trpc";
 
 const Home: NextPage = () => {
   const [url, setUrl] = useState("");
-  const [link, setLink] = useState<Link | null>(null);
+  const [links, setLinks] = useState<Link[]>([]);
   const [copied, setCopied] = useState(false);
 
   const create = api.link.create.useMutation({
-    onSuccess: setLink,
+    onSuccess: (data) => setLinks((links) => [...links, data]),
     onError: () => alert("Could not create link"),
   });
 
-  const onCreate = () => {
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (evt) => {
+    evt.preventDefault();
     create.mutate({ url });
+    setUrl("");
   };
 
   const onCopy = (slug: string) => {
@@ -53,11 +55,14 @@ const Home: NextPage = () => {
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className="flex h-screen bg-gray-50">
-        <div className="m-auto flex w-full max-w-sm flex-col">
+        <div className="m-auto flex w-full max-w-sm flex-col p-4">
           <h1 className="text-center text-xl font-semibold text-slate-700">
             tincy.link
           </h1>
-          <div className="mt-2 flex w-full flex-col rounded bg-white p-4 shadow-lg">
+          <form
+            className="mt-2 flex w-full flex-col rounded bg-white p-4 shadow-lg"
+            onSubmit={onSubmit}
+          >
             <input
               type="url"
               value={url}
@@ -66,33 +71,33 @@ const Home: NextPage = () => {
               className="h-10 rounded px-2"
             />
             <button
-              className="mt-4 h-10 rounded bg-slate-700 text-white"
-              onClick={onCreate}
+              className="mt-4 h-10 rounded bg-slate-700 text-white disabled:cursor-not-allowed disabled:bg-gray-400"
+              type="submit"
               disabled={create.isLoading}
             >
               Create a tincy wincy link!
             </button>
-          </div>
-          {create.isLoading ? (
+          </form>
+          {links.map((link) => (
+            <div
+              key={link.id}
+              className="relative mt-4 cursor-pointer rounded bg-white p-4 shadow-lg"
+              onClick={() => onCopy(link.slug)}
+            >
+              <p className="mb-1">https://tincy.link/{link.slug}</p>
+              <div className="flex items-center gap-1 text-gray-600">
+                <ArrowRight className="h-3 w-3" />
+                <p className="text-sm">{link.url}</p>
+              </div>
+              <Icon className="absolute right-4 top-1/2 h-4 w-4 -translate-y-1/2 transform" />
+            </div>
+          ))}
+          {create.isLoading && (
             <div className="relative mt-4 cursor-pointer rounded bg-white p-4 shadow-lg">
               <div className="mb-3 h-5 w-48 animate-pulse rounded bg-gray-200" />
               <div className="h-4 w-32 animate-pulse rounded bg-gray-200" />
               <div className="absolute right-4 top-1/2 h-4 w-4 -translate-y-1/2 transform animate-pulse rounded bg-gray-200" />
             </div>
-          ) : (
-            link && (
-              <div
-                className="relative mt-4 cursor-pointer rounded bg-white p-4 shadow-lg"
-                onClick={() => onCopy(link.slug)}
-              >
-                <p className="mb-1">https://tincy.link/{link.slug}</p>
-                <div className="flex items-center gap-1 text-gray-600">
-                  <ArrowRight className="h-3 w-3" />
-                  <p className="text-sm">{link.url}</p>
-                </div>
-                <Icon className="absolute right-4 top-1/2 -translate-y-1/2 transform" />
-              </div>
-            )
           )}
         </div>
       </main>
